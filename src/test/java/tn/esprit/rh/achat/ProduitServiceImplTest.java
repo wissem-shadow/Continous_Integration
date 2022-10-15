dde3467e9ea7bf7a8b6925e4c2c10aec6b998d43
riadhmars <riadh.mars@esprit.tn>
2022-10-15 03:26:39 +0200

    First commit from riadh

--- src/test/java/tn/esprit/rh/achat/ProduitServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/ProduitServiceImplTest.java
@@ -1,4 +1,38 @@
 package tn.esprit.rh.achat;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.text.ParseException;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.rh.achat.entities.Produit;
+import tn.esprit.rh.achat.services.IProduitService;
+
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+
 public class ProduitServiceImplTest {
+	@Autowired
+	IProduitService produitService;
+	
+	@Test
+	public void testAddProduit()  throws ParseException{
+		List<Produit> produits = produitService.retrieveAllProduits();
+		Produit produitADD = new Produit().builder()
+				.codeProduit("Stock B")
+				.libelleProduit("toto")
+				.prix(7)
+				.build();
+		int expected = produits.size();
+		Produit savedProduit= produitService.addProduit(produitADD);
+		assertEquals(expected + 1, produitService.retrieveAllProduits().size());
+		assertNotNull(savedProduit.getCodeProduit());
+	
+	} 
 }
