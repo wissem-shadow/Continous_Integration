ce5044024587f6711c0fc2f33e30bdaa8ebdf079
wissepm-shadow <wissem.benhouria@gmail.com>
2022-10-16 01:01:00 +0100

    new fix sonar code

--- src/main/java/tn/esprit/rh/achat/services/FactureServiceImpl.java
+++ src/main/java/tn/esprit/rh/achat/services/FactureServiceImpl.java
@@ -68,16 +68,30 @@ public class FactureServiceImpl implements IFactureService {
 
 	@Override
 	public List<Facture> getFacturesByFournisseur(Long idFournisseur)  {
+		try {
+
+
 		Fournisseur fournisseur = fournisseurRepository.findById(idFournisseur).orElse(null);
 		return (List<Facture>) fournisseur.getFactures();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
 	}
 
 	@Override
 	public void assignOperateurToFacture(Long idOperateur, Long idFacture) {
 		Facture facture = factureRepository.findById(idFacture).orElse(null);
-		Operateur operateur = operateurRepository.findById(idOperateur).orElse(null);
-		operateur.getFactures().add(facture);
-		operateurRepository.save(operateur);
+		try {
+			Operateur operateur = operateurRepository.findById(idOperateur).orElse(null);
+			operateur.getFactures().add(facture);
+			operateurRepository.save(operateur);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
