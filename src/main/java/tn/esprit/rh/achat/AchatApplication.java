d6d951b4c38254715f7bf7e65677f98336de91cc
ChibeniAmine <amine.chibeni@esprit.tn>
2022-10-21 00:36:22 +0100

    Dockerfilecommit

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -3,11 +3,16 @@ package tn.esprit.rh.achat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @EnableScheduling
 @SpringBootApplication
 public class AchatApplication {
 
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Welcome Amine Chibeni !!" ;
+	}
     public static void main(String[] args) {
         SpringApplication.run(AchatApplication.class, args);
     }
--- src/test/java/tn/esprit/rh/achat/OperateurServiceImplMock.java
+++ src/test/java/tn/esprit/rh/achat/OperateurServiceImplMock.java
@@ -14,19 +14,15 @@ import tn.esprit.rh.achat.entities.Operateur;
 import tn.esprit.rh.achat.repositories.OperateurRepository;
 import tn.esprit.rh.achat.services.OperateurServiceImpl;
 
-<<<<<<< HEAD
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doNothing;
-=======
 
-//import static org.junit.Assert.assertThat;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.ArgumentMatchers.anyLong;
-//import static org.mockito.Mockito.doNothing;
->>>>>>> ac0d67f84b5d102bdfdbf1173e175b0b44ef3b30
+
+
 
 import java.util.ArrayList;
 import java.util.List;
